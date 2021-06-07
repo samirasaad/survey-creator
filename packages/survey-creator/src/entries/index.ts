24d85b2a8a4fa7844d48a9fa9d8c09571ea34cab
samira saad <38919683+samirasaad@users.noreply.github.com>
2021-06-07 16:08:04 +0200

    Update index.ts
    adding new localization

--- packages/survey-creator/src/entries/index.ts
+++ packages/survey-creator/src/entries/index.ts
@@ -112,6 +112,7 @@ import "../localization/russian";
 import "../localization/tajik";
 import "../localization/dutch";
 import "../localization/croatian";
+"../localization/arabic";
 
 export {
   Translation,
